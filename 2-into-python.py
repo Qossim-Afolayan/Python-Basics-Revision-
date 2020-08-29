f94610733e4329b07f695aebb1dcecc9eadb06ee
Qossim Afolayan <qossimafolayan@gmail.com>
2020-08-29 22:09:53 +0100

    intro to Python

---
+++ 2-into-python.py
@@ -0,0 +1,54 @@
+#object: string
+"sushi"
+'sushi'
+"warriors123!"
+"$5.00"
+
+"You're right"
+
+'Shakespeare once said "To be or not to be" in one of his plays'
+" "
+""
+''
+
+"5"
+5
+
+"""Hello
+my name is 
+Qossim
+""" #multilines and documentation
+
+#into to Function: we can't spell function without
+#print
+print("I love Rice and Plantain")
+print("I also love juice beverages")
+print("We're are working on python")
+
+print(5)
+print(3 + 4)
+print(10 - 8)
+
+print("3" + "4")
+
+print("3 + 4")
+
+print("a", "b")
+print("c", "d", "e", "f")
+
+print(5 + 3, 2 - 8)
+print(5 + 3, 2 - 8, 3 + 1)
+
+print("Hello", 10, 3.14)
+
+print("ABC", "DEF")
+print("ABC", "DEF", sep="!")
+print("ABC", "DEF", sep=" ")
+
+print("ABC", "DEF")
+print("ABC", "DEF", end="1")
+print("ABC", "DEF", end="\n")
+print("ABC", "DEF", end="@%^")
+
+
+print("ABC", "DEF", end="@%^", sep="#$")
