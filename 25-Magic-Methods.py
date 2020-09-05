4eb614e8c29b89c83a291f9f2e7e87955186dff7
Qossim Afolayan <qossimafolayan@gmail.com>
2020-09-05 11:27:05 +0100

    __getitem__ and __setitem__ magic methods

--- 25-Magic-Methods.py
+++ 25-Magic-Methods.py
@@ -192,6 +192,99 @@ l2 = Library("Animal Farm", "The Great Gastby")
 print(len(l1))
 print(len(l2))
 
+#indexing with the getitem and setitem magic methods
+pillows = {
+    "soft": 79.99,
+    "hard": 99.99
+}
+
+print(pillows["soft"])
+print(pillows.__getitem__("soft"))
+
+class CrayonBox():
+    def __init__(self):
+        self.crayons = []
+    
+    def add(self, crayon):
+        self.crayons.append(crayon)
+
+    def __str__(self):
+        return "Yippie"
+
+    def __getitem__(self, index):
+        return self.crayons[index]
+    
+    def __setitem__(self, index, value):
+        self.crayons[index] = value
+
+cb = CrayonBox()
+cb.add("Blue")
+cb.add("Yellow")
+print(cb)
+print(cb.__str__())
+print(cb[1])
+
+cb[0] = "Red"
+print(cb[0])
+
+for crayon in cb:
+    print(crayon)
+
+#Exercise
+# Define a Car class that accepts a maker (string), model (string),
+# and year (number) parameters and assigns them to respective
+# attributes
+
+# Define a Dealership class. Each Dealership object should instantiate
+# with a "cars" attribute set to an empty list.
+
+# A Dealership object should have a accept_delivery instance method
+# that accepts a Car object and adds it to the Dealership's internal
+# "cars" list
+
+# Indexing into a Dealership with a number should access a specific
+# Car object in the Dealership.
+
+# An index position in a Dealership should also be overwriteable
+# with a new Car object (see examples below)
+
+class Car():
+    def __init__(self, maker, model, year):
+        self.maker = maker
+        self.model = model
+        self.year = year
+
+class Dealership():
+    def __init__(self):
+        self.cars = []
+
+    def accept_delivery(self, car):
+        self.cars.append(car)
+
+    def __getitem__(self, index):
+        return self.cars[index]
+    
+    def __setitem__(self, index, value):
+        self.cars[index] = value
+
+f150 = Car(maker = "Ford", model = "F-150", year = 2019)
+camry = Car(maker = "Toyota", model = "Camry", year = 2020)
+porsche = Car (maker = "Porsche", model = "911 Carrera", year = 2021)
+
+dealership = Dealership()
+
+dealership.accept_delivery(f150)
+dealership.accept_delivery(camry)
+
+print(dealership[0].year) # 2019 -- the F150's year
+
+dealership[0] = porsche
+
+for car in dealership:
+  print(car.maker) # Porsche, Toyota
+
+
+
 
 
 
