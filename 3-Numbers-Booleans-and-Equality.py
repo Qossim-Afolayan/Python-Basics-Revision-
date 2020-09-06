402c375bed2372781095f3034c05fca138646975
Qossim Afolayan <qossimafolayan@gmail.com>
2020-09-06 14:13:59 +0100

    Type Function

--- 3-Numbers-Booleans-and-Equality.py
+++ 3-Numbers-Booleans-and-Equality.py
@@ -75,3 +75,74 @@ print(5 >= 7)
 print(5 < 8 <= 10)
 print(5 < 10 <= 2)
 
+#Type Function
+print(type(5))
+print(type(10))
+
+print(type(3.7))
+print(type(2.0))
+
+print(type("Computer"))
+print(type(""))
+
+print(type(5) == type(10))
+print(type("Qossim") == type("Hassan"))
+print(type(True))
+
+print(type([1, 2, 3]))
+print(type({"NJ": "Trenton"}))
+
+#Type Conversion
+print(int(3.14))
+print(int(3.99))
+print(int(float(("3.99"))))
+print(int(3))
+
+print(float(3))
+print(float("10.98"))
+print(float(5.23))
+
+print(str(3.67))
+print(str(5))
+print("Hello")
+
+print(str(5) + "Hello")
+
+print(3 + 4)
+print(4.5 + 5.5)
+print(3 + 5.9)
+
+#Variable
+name = "Qossim"
+age = 12
+handsome = True
+favorite_language = "Python"
+
+print(name)
+print(handsome)
+# print(occupation)
+
+print(age + 4)
+print("My name is", name, "and I am", age, "years old")
+
+age = 10
+print(age)
+
+age = 27 + 10
+print(age)
+
+fact_or_fiction = 6 < 19
+print(fact_or_fiction)
+
+chameleon = 5
+print(chameleon)
+
+chameleon = "Some String"
+print(chamelean)
+
+chameleon = 23.5
+print(chameleon)
+
+chameleon = False
+print(chameleon)
+
