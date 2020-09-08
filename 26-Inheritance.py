335d6109bd83a783ae4f73e7cc70881713e13241
Qossim Afolayan <qossimafolayan@gmail.com>
2020-09-08 09:37:34 +0100

    super function

--- 26-Inheritance.py
+++ 26-Inheritance.py
@@ -101,3 +101,89 @@ socks = Socks()
 
 print(socks.sell())
 print(clothing.sell())
+
+#Super function
+class Animal():
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        return F"{self.name} is enjoying the {food}"
+
+class Cat(Animal):
+    def __init__(self, name, sleep):
+        super().__init__(name)
+        self.sleep = sleep
+
+hurayra = Cat("hurayra", 10)
+print(hurayra.name)
+print(hurayra.sleep)
+
+#Exercise
+# Declare a Musician class that accepts a name argument in its initialization.
+# The initialization should assign a name argument to a name attribute. 
+# In addition, each Musician object should have an "albums" attribute 
+# that begins as an empty list.
+# Define a release_album instance method on a Musician that accepts a title (string). 
+# It should append the string to the end of the list held by the
+# albums attribute (see examples below).
+#
+# Declare a Drummer subclass than inherits from the Musician superclass. 
+# In addition to a name, a drummer should also have a stamina attribute 
+# represented by an integer. 
+# The subclass should invoke the Musician superclass’s initialization procedure
+# and pass it the drummer’s name.
+# It should also set the stamina attribute in its own initialization process.
+#
+# EXAMPLE:
+# lars = Drummer(name = "Lars", stamina = 2)
+# print(lars.name)   # "Lars"
+# print(lars.stamina) # 2
+# print(lars.albums) # []
+#
+# lars.release_album("Ride the Lightning")
+# print(lars.albums) # ["Ride the Lightning"]
+#
+# lars.release_album("Master of Puppets")
+# print(lars.albums)  # ["Ride the Lightning", 'Master of Puppets']
+
+class Musician():
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+
+    def release_album(self,title):
+        return self.albums.append(title)
+
+class Drummer(Musician):
+    def __init__(self, name, stamina):
+        super().__init__(name)
+        self.stamina = stamina
+
+lars = Drummer(name = "Lars", stamina = 2)
+print(lars.name)   # "Lars"
+print(lars.stamina) # 2
+print(lars.albums) # []
+
+lars.release_album("Ride the Lightning")
+print(lars.albums) # ["Ride the Lightning"]
+
+lars.release_album("Master of Puppets")
+print(lars.albums)  # ["Ride the Lightning", 'Master of Puppets']
+
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year % 4 == 0 and year % 100 == 0 and year % 400 ==0:
+        leap = True
+
+    else: 
+        leap = False
+    
+    return leap
+year = 1800
+print(is_leap(year))
+
+
+
