11805810522739c9a04eadda11b0a4528e2fe759
Qossim Afolayan <qossimafolayan@gmail.com>
2020-09-25 09:46:11 +0100

    New files

---
+++ 29-The-Random-Module.py
@@ -0,0 +1,14 @@
+#The random Module: The random, randint and randrange function
+import random
+
+print(random.random())
+
+print(random.random() * 100)
+
+print(random.randint(1, 10))
+
+print(random.randrange(0, 50, 10))
+
+#The choice and the sample function
+print(random.choice(["Qossim", "Gbolahan", "Afolayan"]))
+print(random.choice([1, 2, 3, 4]))
